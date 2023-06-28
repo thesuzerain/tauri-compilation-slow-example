7921a5ca1f3928b9fc4ad82251fd3bd37b8d8b4f
thesuzerain <wverchere@gmail.com>
2023-06-28 12:55:54 -0700

    removed macros

--- theseus_gui/src-tauri/src/main.rs
+++ theseus_gui/src-tauri/src/main.rs
@@ -13,12 +13,19 @@ fn main() {
             api::jre::jre_get_all_jre,
             api::jre::jre_find_jre_18plus_jres,
             api::jre::jre_find_jre_19plus_jres,
-            // api::jre::jre_find_jre_20plus_jres,
-            // api::jre::jre_find_jre_21plus_jres,
-            // api::jre::jre_find_jre_22plus_jres,
-            // api::jre::jre_find_jre_23plus_jres,
-            // api::jre::jre_find_jre_24plus_jres,
-            // api::jre::jre_find_jre_25plus_jres,
+            api::jre::jre_find_jre_20plus_jres,
+            api::jre::jre_find_jre_21plus_jres,
+            api::jre::jre_find_jre_22plus_jres,
+            api::jre::jre_find_jre_23plus_jres,
+            api::jre::jre_find_jre_24plus_jres,
+            api::jre::jre_find_jre_25plus_jres,
+            api::jre::jre_find_jre_26plus_jres,
+            api::jre::jre_find_jre_27plus_jres,
+            api::jre::jre_find_jre_28plus_jres,
+            api::jre::jre_find_jre_29plus_jres,
+            api::jre::jre_find_jre_30plus_jres,
+
+
             api::jre::jre_find_jre_17_jres,
         ])
         .run(tauri::generate_context!())
